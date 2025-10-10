9d9be2f3646ae16c240bf33f97ef4d917d76e49a
Vanshika4104 <141626227+Vanshika4104@users.noreply.github.com>
2025-10-10 20:12:55 +0530

    Time: 4 ms (76.2%), Space: 44.5 MB (50.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,25 +1,22 @@
 class Solution {
-    public static String SortString(String str){
-        char[] c=str.toCharArray();
-        Arrays.sort(c);
-        return new String(c);
-    }
     public boolean isAnagram(String s, String t) {
         int n=s.length();
         int m=t.length();
 
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
         if(n!=m){
             return false;
         }
-        s=SortString(s);
-        t=SortString(t);
-        for(int i=0; i<n;i++){
-            if(s.charAt(i)!= t.charAt(i)){
+        for(int i=0; i<n ; i++){
+            if(sArr[i] !=tArr[i]){
                 return false;
             }
-
         }
         return true;
-
     }
 }
