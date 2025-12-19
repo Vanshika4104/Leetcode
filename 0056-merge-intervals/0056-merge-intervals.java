9df868f8c183ba0c0dca63ea7f29dfbbdb09c7e6
Vanshika4104 <141626227+Vanshika4104@users.noreply.github.com>
2025-12-20 01:40:42 +0530

    Time: 8 ms (88.66%), Space: 49.5 MB (7.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length == 0) return new int[0][];
+        if(intervals.length==0) return new int[0][];
+        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));
+        List<int[]> res= new ArrayList<>();
+        int[] current= intervals[0];
 
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
-        List <int []> res= new ArrayList<>();
-
-        int [] current= intervals[0];
-
-        for(int i=1; i< intervals.length ; i++){
-            if(intervals[i][0] <= current[1]){
+        for(int i=1; i<intervals.length;i++){
+            if(intervals[i][0]<= current[1]){
                 current[1]=Math.max(intervals[i][1], current[1]);
             }else{
                 res.add(current);
@@ -16,8 +14,6 @@ class Solution {
             }
         }
         res.add(current);
-
         return res.toArray(new int[res.size()][]);
-
     }
 }
